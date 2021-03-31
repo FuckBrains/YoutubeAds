5d2ab4a0e3dfaeb63c74b77e1a3a0f23e763bf56
Camq543 <cameron.ballard543@gmail.com>
2021-03-31 19:00:35 -0400

    added a -

--- firstTests.py
+++ firstTests.py
@@ -484,7 +484,7 @@ def main():
 
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
-    options.add_argument("-headless")
+    options.add_argument("--headless")
     options.add_argument("--mute-audio")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
